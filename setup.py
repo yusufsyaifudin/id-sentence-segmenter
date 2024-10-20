75187cba89e9017052ce9a947bc970b4f8664c3e
Yusuf <yusuf.syaifudin@gmail.com>
2024-10-20 22:49:36 +0700

    feat(setup.py): add setup.py for fallback

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+    setup(
+        name='id-sentence-segmenter',
+        version='0.0.1',
+        author='Yusuf Syaifudin',
+        author_email='yusuf.syaifudin@gmail.com',
+        description="A distributed unique ID generator inspired by Twitter's Snowflake.",
+        url="https://github.com/hjpotter92/sonyflake-py",
+        packages=find_packages(),
+        package_data={"id-sentence-segmenter": ["res/lexicon/*.txt", "res/wordlists/*.txt"]},
+        include_package_data=True,
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Natural Language Processing :: Indonesian Language",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3 :: Only",
+        ],
+        python_requires=">=3.6",
+        project_urls={
+            "Documentation": "https://github.com/yusufsyaifudin/id-sentence-segmenter",
+        },
+        tests_require=(),
+    )
